a4fcfb96180c75778f274f1b71222b9810c2b580
Daniel Lorigan <dlorigan@uw.edu>
2023-04-28 14:49:36 -0700

    Try to solve /ips 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			input: {
+				ips: resolve(__dirname, 'ips/index.html')
+			}
+		}
+	},
 	server: {
 		host: true,
 		port: 8080
