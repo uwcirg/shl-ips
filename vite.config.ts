30118cfc29c6126a30f087898c055d6edb16ef93
Daniel Lorigan <dlorigan@uw.edu>
2023-05-02 12:40:10 -0700

    Try fix for ips url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$ips: mode === "production" ? "/ips" : "/ips/index.html",
+		},
+	},
 	server: {
 		host: true,
 		port: 8080
 	}
-});
+}));
