53acfafb8b34662f0856c42a698bf5d6ecfc883d
Daniel Lorigan <daniellrgn@gmail.com>
2023-05-04 21:37:22 -0700

    Update banner and convert to svelte for app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,21 @@
   import { setContext } from 'svelte';
   import { writable } from 'svelte/store';
   import {
-    ButtonDropdown,
+    Dropdown,
     Col,
+    Collapse,
     Container,
     DropdownItem,
     DropdownMenu,
     DropdownToggle,
     Icon,
+    Image,
+    Nav,
     Navbar,
     NavbarBrand,
+    NavItem,
+    NavLink,
+    NavbarToggler,
     Row,
     Styles
   } from 'sveltestrap';
@@ -32,37 +38,118 @@
 
   let reset = writable(0);
   setContext('reset', reset);
+
+  let isOpen = false;
+  function handleUpdate(event) {
+    isOpen = event.detail.isOpen;
+  }
 </script>
 
 <Container class="main" fluid>
+<Styles />
+<Navbar color="light" light expand="md">
+  <NavbarBrand>
+    <Image class="doh_logo_doh-black.png" alt="Washington State Department of Health Logo" width="240" src="/doh_logo_doh-black.png"/>
+  </NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink>Login</NavLink>
+      </NavItem>
+      <Dropdown nav inNavbar size="sm" direction="down">
+        <DropdownToggle color="primary" nav caret>Actions</DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem href="/create">Add New SHLink</DropdownItem>
+          <DropdownItem
+            on:click={() => {
+              delete window.localStorage[LOCAL_STORAGE_KEY];
+              goto('/');
+            }}>Reset Demo</DropdownItem>
+          {#if $shlStore.length > 0}
+            <DropdownItem divider />
+            <DropdownItem header>View Stored SHLinks</DropdownItem>
+            {#each $shlStore as shl, i}
+              <DropdownItem href={'/view/' + shl.id}>{shl.label || `SHLink ${i + 1}`}</DropdownItem>
+            {/each}
+          {/if}
+        </DropdownMenu>
+      </Dropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
+<Row>
+  <Col>
+    <Image alt="WA Verify Logo" width="200" src="/waverifylogo.png" style="align-self: center"></Image>
+    <div style="vertical-align: middle; font-size: 18px; display: inline-block; padding-left: 17px; font-family: Verdana, sans-serif; color: rgb(34, 72, 156);">WA Verify International Patient Summary Viewer</div>
+  </Col>
+</Row>
+<Row class="main-row">
+  <Col>
+    <slot />
+  </Col>
+</Row>
+<Row>
+  <Col>
+    <footer>
+      This demonstration shows how to create a <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://docs.smarthealthit.org/smart-health-links/user-stories">SMART Health Link</a
+      >
+      for any FHIR
+      <a href="https://build.fhir.org/ig/HL7/fhir-ips/" target="_blank" rel="noreferrer"
+        >International Patient Summary</a
+      >
+      document. SHLinks can be shared by copy/paste, or by presenting a QR. Source at
+      <a href="https://github.com/jmandel/shlips" target="_blank" rel="noreferrer"
+        >github.com/jmandel/shlips</a
+      >.
+    </footer>
+  </Col>
+</Row>
+</Container>
+
+<!-- <Container class="main" fluid>
   <Styles />
   <Row>
+    <Navbar style="background-color: rgb(249, 249, 249); border-bottom: 1px solid rgb(204, 204, 204);">
+      <NavbarBrand>
+        <Image class="doh_logo_doh-black.png" alt="Washington State Department of Health Logo" width="240" src="/doh_logo_doh-black.png"/>
+      </NavbarBrand>
+      <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+          <NavItem>
+            <NavLink>Login</NavLink>
+          </NavItem>
+          <Dropdown nav inNavbar size="sm" direction="down">
+            <DropdownToggle color="primary" nav caret>Actions...</DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem href="/create">Add New SHLink</DropdownItem>
+              <DropdownItem
+                on:click={() => {
+                  delete window.localStorage[LOCAL_STORAGE_KEY];
+                  goto('/');
+                }}>Reset Demo</DropdownItem>
+              {#if $shlStore.length > 0}
+                <DropdownItem divider />
+                <DropdownItem header>View Stored SHLinks</DropdownItem>
+                {#each $shlStore as shl, i}
+                  <DropdownItem href={'/view/' + shl.id}>{shl.label || `SHLink ${i + 1}`}</DropdownItem>
+                {/each}
+              {/if}
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  </Row>
+  <Row>
     <Col>
-      <Navbar>
-        <NavbarBrand href="/">SMART Health Links for IPS</NavbarBrand>
-        <ButtonDropdown size="sm" direction="down">
-          <DropdownToggle color="primary" caret>Actions...</DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem href="/create">Add New SHLink</DropdownItem>
-            <DropdownItem
-              on:click={() => {
-                delete window.localStorage[LOCAL_STORAGE_KEY];
-                goto('/');
-              }}>Reset Demo</DropdownItem
-            >
-            {#if $shlStore.length > 0}
-              <DropdownItem divider />
-              <DropdownItem header>Stored SHLinks</DropdownItem>
-              {#each $shlStore as shl, i}
-                <DropdownItem href={'/view/' + shl.id}
-                  ><Icon name="eye" />
-                  {shl.label || `SHLink ${i + 1}`}</DropdownItem
-                >
-              {/each}
-            {/if}
-          </DropdownMenu>
-        </ButtonDropdown>
-      </Navbar>
+      <Image alt="WA Verify Logo" width="200" src="/waverifylogo.png" style="align-self: center"></Image>
+    </Col>
+    <Col>
+      <div style="vertical-align: middle; font-size: 18px; display: inline-block; padding-left: 17px; font-family: Verdana, sans-serif; color: rgb(34, 72, 156);">WA Verify International Patient Summary Viewer</div>
     </Col>
   </Row>
   <Row class="main-row">
@@ -89,7 +176,7 @@
       </footer>
     </Col>
   </Row>
-</Container>
+</Container> -->
 
 <style>
   :global(.main-row) {
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,17 @@
+<html lang="EN-US">
+	<body>
+		<div>
+			<h1 style="padding-left: 17px; font-size: 38px; font-weight: 200; font-family: Verdana, sans-serif; color: rgb(200, 76, 14); margin: 50px 0px;">WA Verify International Patient Summary Portal</h1>
+			<span style="font-size: 18px; font-weight: 200; font-family: Verdana, sans-serif; margin: 50px 0px;">
+				<p style="padding-left: 17px;">Welcome to the Digital IPS (International Patient Summary) Portal. WA Verify is a free tool that can be used to access and share your digital records via SMART Health Link (SHLink). Enter a few details to generate a QR Code to access your records. If you would like to share your records, you may use either the electronic or a printed version of the QR Code you’ll get from the system. We recommend waiting 3-7 days for any new vaccination dose to show up in the Immunization Registry before generating a new SHLink.</p>
+
+				<p style="padding-left: 17px;">People within a family may have multiple verification records associated with a single phone number or email address, enter each digital verification record request separately.</p>
+
+				<p style="padding-left: 17px;">The system provides a digital copy of state records. If you received your vaccinations from a federal agency (Department of Defense, Indian Health Services, or Veterans Affairs), you will need to contact those agencies for assistance.</p>
+
+				<p style="padding-left: 17px;">If you have questions about your Digital IPS, visit the <a href="aspot">Frequently Asked Questions (FAQ) page</a>.</p>
+			</span>
+		</div>
+		<hr>
+	</body>
+</html>
