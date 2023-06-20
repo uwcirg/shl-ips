1303a97ae8990c2e6d0dd03611d4a1de1d60b138
Bill Lober <lober@uw.edu>
2023-06-20 14:40:34 -0600

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -39,9 +39,9 @@
 		</p>
 
 
-<p>WA Verify<sup>+</sup> WA allows people to access augment, and electively share their own Intternational Patient Summaries (IPS), a richer set of health information collected and stored on their behalf by the Washington State Department of Health. IPS data include critical clinical data, immunizations, advance directives, and other data needed for urgent medical care. This data may be helpful to those traveling away from home, to parents and caregivers, and to anyone who wants to be able to see their own records, or securely share their data with healthcare providers or others of their choosing.  WA Verify<sup>+</sup> uses the secure, patient-controlled SMART Health Link (SHL) standard. </p>
+<p>WA Verify<sup>+</sup> allows people to access augment, and electively share their own health data using the Intternational Patient Summary (IPS) standard. IPS data include important clinical data, immunizations, advance directives, and other data useful for urgent medical care. This data may be helpful to those traveling, to parents or caregivers, and to anyone who wants to be able to see their own records, or securely share their data with healthcare providers or others of their choosing.  WA Verify<sup>+</sup> uses the secure, patient-controlled SMART Health Link (SHL) standard to allow people to share information collected and stored on their behalf by the Washington State Department of Health. 
 
-		<p>This system generates QR codes and links to demonstration the ability of WA Verify<sup>+</sup> to facilitate sharing directly controlled by individuals. You can see a list of SHLs generated <i>on this device</i>, as well as generate a new one.</p> 
+		<p>This system generates QR codes and links which demonstrate the ability of WA Verify<sup>+</sup> to facilitate sharing directly controlled by individuals. You can see a list of SHLs generated <i>on this device</i>, as well as generate a new one.</p> 
 	</span>
 </div>
 <Row style="padding: 0px 17px">
