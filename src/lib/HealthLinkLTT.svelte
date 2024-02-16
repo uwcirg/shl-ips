c0b51c36b78d681e7451fc457adf075d075cf3c7
Daniel Lorigan <daniellrgn@gmail.com>
2024-02-15 18:37:51 -0800

    Update shl management view

--- src/lib/HealthLinkLTT.svelte
+++ src/lib/HealthLinkLTT.svelte
@@ -6,10 +6,8 @@
     Card,
     CardBody,
     CardFooter,
-    CardHeader,
-    CardSubtitle,
+    CardImg,
     CardText,
-    CardTitle,
     Col,
     FormGroup,
     Icon,
@@ -26,35 +24,41 @@
   import type { Writable } from 'svelte/store';
   import type { SHLAdminParams, SHLClient } from './managementClient';
 
-  export let shl: SHLAdminParams;
-  let shlControlled: SHLAdminParams;
   let open = false;
   const toggle = () => (open = !open);
 
-  function syncProps(shl: SHLAdminParams) {
-    shlControlled = JSON.parse(JSON.stringify(shl));
-  }
-  $: syncProps(shl);
-
-  let shlStore: Writable<SHLAdminParams[]> = getContext('shlStore');
+  let shlStore: Writable<SHLAdminParams> = getContext('shlStore');
   let shlClient: SHLClient = getContext('shlClient');
 
   let copyNotice = '';
 
   let href: Promise<string>;
   let qrCode: Promise<string>;
-  let showPassword = false;
-  $: type = showPassword ? 'text' : 'password';
-  $: icon = showPassword ? 'eye-slash-fill' : 'eye-fill';
+  let exp: Date;
+  let today: Date;
+  let expDisplay: string;
+  let newExpiration: number = 60 * 60;
+  let inactive = false;
+
   $: {
-    href = getUrl(shl);
+    href = getUrl($shlStore);
   }
 
   $: {
-    qrCode = href.then((r) => QRCode.toDataURL(r, { errorCorrectionLevel: 'M' }));
+    qrCode = href.then((r) => QRCode.toDataURL(r, { errorCorrectionLevel: 'M' }))
   }
 
-  let canShare = navigator?.canShare?.({ url: 'https://example.com', title: 'Title' });
+  $: {
+    if ($shlStore.exp) {
+      exp = new Date($shlStore.exp * 1000);
+      today = new Date();
+      expDisplay = exp.toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      });
+    }
+  }
 
   async function getUrl(shl: SHLAdminParams) {
     let shlMin = {
@@ -62,14 +66,14 @@
       managementToken: shl.managementToken,
       encryptionKey: shl.encryptionKey,
       files: []
-    }
+    };
     return await shlClient.toLink(shlMin);
   }
 
   async function copyShl() {
     let copyNoticePrev = copyNotice;
     copyNotice = '...';
-    let text = await getUrl(shl);
+    let text = await getUrl($shlStore);
     navigator.clipboard.writeText(text);
     copyNotice = 'Copied!';
     setTimeout(() => {
@@ -77,148 +81,147 @@
     }, 1000);
   }
 
-  async function deleteShl() {
-    shlClient.deleteShl(shl);
-    $shlStore = $shlStore.filter((l) => l.id !== shl.id);
+  async function deactivateShl() {
+    shlClient.deleteShl($shlStore);
     toggle();
-    goto('/');
+    // TODO: Implement post-deactivation flow
+    goto('/share');
   }
 
-  async function addFile() {
-    goto(`/create?shlid=${shl.id}`);
-  }
-
-  async function deleteFile(fileContent:string) {
-    shl = await shlClient.deleteFile(shl, fileContent).then((shl) => {
-      let updatedFiles = shl.files.filter((f) => f.contentEncrypted !== fileContent);
-      shl.files = updatedFiles;
-      return shl;
-    });
-    $shlStore[$shlStore.findIndex(obj => obj.id === shl.id)] = shl;
-  }
+  async function renewShl() {}
 </script>
-<Row cols={{ md: 2, sm: 1 }}>
-  <Col>
-    <Card class="mb-3" color="light">
-      <CardHeader>
-        <CardTitle>
-          <Icon name={shl.passcode ? 'lock' : 'unlock'} />
-          {shl.label}</CardTitle
-        >
-      </CardHeader>
-      <CardBody>
-        {#if shl.exp}
-          <CardSubtitle color="success">
-            Expires: {new Date(shl.exp * 1000).toISOString().slice(0, 10)}
-          </CardSubtitle>
-        {/if}
-    
-        <CardText>
-          {#await qrCode then dataUrl}
-            <p class="logo">
-              <img class="qr" alt="QR Code for SHL" src={dataUrl} />
-              <img class="logo" alt="Let's Talk Tech Logo" src='/img/ltt-logo.svg' />
-            </p>
-          {/await}
-        </CardText>
-      </CardBody>
-      <CardFooter>
-        {#if canShare}
-          <Button
-            size="sm"
-            color="success"
-            on:click={async () => {
-              navigator.share({ url: await href, title: shl.label });
-            }}><Icon name="share" /> Share</Button
-          >
-        {/if}
-        <Button size="sm" color="success" on:click={copyShl} disabled={!!copyNotice}>
-          <Icon name="clipboard" />
-          {#if copyNotice}
-            {copyNotice}
-          {:else}
-            Copy Link
+
+<Row class="justify-content-center">
+  <Col sm="8">
+    <!-- <h2>{$shlStore.label}</h2> -->
+    <h2>Let's Talk Tech Choices Report (November 29, 2023)</h2>
+    <h3>Share Report</h3>
+    <p>There are 2 ways to share this electronically.</p>
+    <ol>
+      <li>
+        <strong>Email it:</strong> You can copy a link and paste it into an email to send.
+        <ul>
+          <li>First, to copy the link, click the copy button below:</li>
+          <Row class="justify-content-center">
+            <Col xs="5">
+              <Button size="sm" color="success" on:click={copyShl} disabled={!!copyNotice}>
+                <Icon name="clipboard" />
+                {#if copyNotice}
+                  {copyNotice}
+                {:else}
+                  Copy Link
+                {/if}
+              </Button>
+            </Col>
+          </Row>
+          <li>
+            Then, open your email and start a new message to the person you want to send it to.
+          </li>
+          <li>
+            Paste the link into the body of the email. Here’s how: After you have clicked the button
+            above, right-click, then choose “Paste” from the pop-up menu. Or, if you’re using a PC,
+            press the Ctrl and V buttons at the same time. On a Mac, press Command and V.
+          </li>
+          <li>
+            Send the email. The person you send it to will receive the link to be able to see your
+            saved Choices Report.
+          </li>
+        </ul>
+      </li>
+      <li>
+        <strong>Share a QR code:</strong> Point your phone’s camera at the QR code so that it's clearly
+        visible within your smartphone's screen. The phone automatically scans the code. On some QR readers,
+        you have to tap a button to scan the code. If necessary, tap the button. Your smartphone
+        reads the code and navigates to a page showing your Choices Summary.
+      </li>
+    </ol>
+    <Row class="justify-content-center mx-4" cols={{ xs: 1, md: 2 }}>
+      <Col xs=12 md=7>
+        <Row>
+          <Card class="mb-2 p-0" color="light">
+            <CardBody>
+              <CardText>
+                {#await qrCode then qrImage}
+                  <CardImg class="img-fluid" alt="QR Code for SHL" src={qrImage} />
+                  <CardImg
+                    style="position: absolute;
+              background: #325c33;
+              width: 110px;
+              height: 27px;
+              left: calc(50% - 55px);
+              top: calc(50% - 2em);
+              border: 5px solid #325c33;
+              box-sizing: border-box;"
+                    class="logo"
+                    alt="Let's Talk Tech Logo"
+                    src="/img/ltt-logo.svg"
+                  />
+                  <!-- 
+                  <img class="qr" alt="QR Code for SHL" src={dataUrl} />
+                  <img class="logo" alt="Let's Talk Tech Logo" src='/img/ltt-logo.svg' />
+                </p> -->
+                {/await}
+              </CardText>
+            </CardBody>
+            <CardFooter>
+              {#await href then href}
+                <Row class="justify-content-center mx-auto">
+                  <Button size="sm" color="success" {href} target="_blank">
+                    <Icon name="box-arrow-up-right" /> View Summary
+                  </Button>
+                </Row>
+              {/await}
+            </CardFooter>
+          </Card>
+        </Row>
+      </Col>
+      <Col xs=12 md=5>
+        <Row class="mt-1">
+          {#if expDisplay}
+            {#if exp > today}
+              Expires {expDisplay}
+            {:else}
+              <span class="text-danger">Expired on {expDisplay}</span>
+            {/if}
+            <FormGroup>
+              <Label>Renew summary link?</Label>
+              <Input type="radio" bind:group={newExpiration} value={60 * 60} label="1 hour" />
+              <Input
+                type="radio"
+                bind:group={newExpiration}
+                value={60 * 60 * 24 * 7}
+                label="1 week"
+              />
+              <Input
+                type="radio"
+                bind:group={newExpiration}
+                value={60 * 60 * 24 * 7 * 365}
+                label="1 year"
+              />
+              <br />
+              <Button size="sm" on:click={renewShl} color="primary">Renew Expiration</Button>
+            </FormGroup>
           {/if}
-        </Button>
-        {#await href then href}
-          <Button size="sm" color="success" {href} target="_blank">
-            <Icon name="box-arrow-up-right" /> View IPS
-          </Button>
-        {/await}
-      </CardFooter>
-    </Card>
-  </Col>
-  <Col>
-    <FormGroup class="label shlbutton">
-      <Label for="label">Label for SMART Health Link</Label>
-      <Input
-        name="label"
-        maxlength={40}
-        type="text"
-        bind:value={shlControlled.label}
-        placeholder="label"
-      />
-      <Button
-        size="sm"
-        color="secondary"
-        disabled={(shl.label || '') === (shlControlled.label || '')}
-        on:click={async () => {
-          $shlStore = $shlStore.map((e) => {
-            if (e.id === shl.id) {
-              shl = { ...shl, label: shlControlled.label };
-              return shl;
-            } else {
-              return e;
-            }
-          });
-        }}>
-        <Icon name="sticky" /> Update Label
-      </Button>
-    </FormGroup>
-    <FormGroup class="passcode shlbutton">
-      <Label for="passcode">Add or Update Passcode (optional)</Label>
-      <div style="position:relative">
-        <Input
-          maxlength={40}
-          name="passcode"
-          type={type}
-          bind:value={shlControlled.passcode}
-          placeholder="Assign Passcode"
-        />
-        <Icon name={icon}
-          style="position: absolute;
-          cursor: pointer;
-          height: 25px;
-          width: 20px;
-          top: 6px;
-          right: 10px;
-          color: rgb(50, 50, 50);"
-          onclick={() => showPassword = !showPassword}/>
-      </div>
-      <Button
-        size="sm"
-        color="secondary"
-        disabled={(shl.passcode || '') === (shlControlled.passcode || '')}
-        on:click={async () => {
-          await shlClient.resetShl({ ...shl, passcode: shlControlled.passcode });
-          $shlStore = $shlStore.map((e) =>
-            e.id === shl.id ? { ...shl, passcode: shlControlled.passcode } : e
-          );
-        }}><Icon name="lock" /> Update Passcode</Button>
-    </FormGroup>
-    <FormGroup class="shlbutton">
-      <Button size="sm" on:click={toggle} color="danger">Delete SMART Health Link</Button>
-      <Modal isOpen={open} backdrop="static" {toggle}>
-        <ModalHeader {toggle}>Delete SMART Health Link</ModalHeader>
-        <ModalBody>
-          "{shl.label}" will be permanently deleted. Continue?
-        </ModalBody>
-        <ModalFooter>
-          <Button color="danger"  on:click={deleteShl}><Icon name="trash" /> Delete SHL</Button>
-          <Button color="secondary" on:click={toggle}>Cancel</Button>
-        </ModalFooter>
-      </Modal>
-    </FormGroup>
+        </Row>
+      </Col>
+    </Row>
+    <Row class="justify-content-center m-4">
+      <Col xs=12 class="pt-4" style="border-top:1px solid rgb(204,204,204)">
+        <Button size="sm" style="width:100%" on:click={toggle} color="danger">Deactivate Summary Link</Button>
+        <Modal isOpen={open} backdrop="static" {toggle}>
+          <ModalHeader {toggle}>Deactivate Summary Link</ModalHeader>
+          <ModalBody>
+            The link to view "{$shlStore.label}" will be deactivated, and those it was shared with will no longer
+            be able to view its contents. A new link will be generated for you that you may share.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" on:click={toggle}>Cancel</Button>
+            <Button color="danger" on:click={deactivateShl}>Confirm</Button>
+          </ModalFooter>
+        </Modal>
+      </Col>
+    </Row>
+    <p><strong>If you have any questions or problems using the system, please get in touch at ?clara@email.com? for assistance.</strong></p>
   </Col>
 </Row>
 
@@ -231,14 +234,14 @@
     width: 250px;
     height: 250px;
   }
-  img.logo {
+  .logo {
     position: absolute;
     background: #325c33;
     width: 110px;
-    height: 25px;
+    height: 27px;
     /* padding: 2px; */
     left: calc(50% - 55px);
-    top: calc(50% - 1em);
+    top: calc(50% - 2em);
     border: 5px solid #325c33;
     box-sizing: border-box;
   }
@@ -255,5 +258,23 @@
   :global(.card-title) {
     font-size: 1em;
     font-weight: bold;
+  }
+
+  li {
+    margin-bottom: 5px;
+    margin-top: 5px;
+  }
+
+  /* Style for the ordered list */
+  ol > li::marker {
+    font-weight: bold;
+  }
+
+  /* Style for the secondary-level bulleted items */
+  ul {
+    list-style-type: disc; /* Use filled-in bullet points */
+    margin-top: 0; /* Remove default margin */
+    margin-bottom: 10px; /* Add some spacing between lists */
+    padding-left: 20px; /* Indent the bullets */
   }
 </style>
