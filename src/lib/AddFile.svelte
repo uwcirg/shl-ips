9031eb16e21767d6c92fb24e1bc15abe7dc57fcd
Daniel Lorigan <daniellrgn@gmail.com>
2024-02-29 22:55:04 -0800

    Handle fetch accordion, tab switching, and add success message

--- src/lib/AddFile.svelte
+++ src/lib/AddFile.svelte
@@ -1,242 +1,313 @@
 <script lang="ts">
-    import * as jose from 'jose';
-    import * as pako from 'pako';
-    import { createEventDispatcher, onMount } from 'svelte';
-    import {
-        Button,
-        Card,
-        CardBody,
-        CardText,
-        Col,
-        FormGroup,
-        Icon,
-        Input,
-        Label,
-        Row,
-        Spinner,
-        TabContent,
-        TabPane } from 'sveltestrap';
-    import FetchUrl from './FetchUrl.svelte';
-    import FetchFile from './FetchFile.svelte';
-    import FetchSoF from './FetchSoF.svelte';
-    import ResourceSelector from './ResourceSelector.svelte';
-    import { verify } from './shc-decoder.js';
+  import * as jose from 'jose';
+  import * as pako from 'pako';
+  import { createEventDispatcher, onMount } from 'svelte';
+  import {
+    Accordion,
+    AccordionItem,
+    Button,
+    Card,
+    CardBody,
+    CardText,
+    Col,
+    FormGroup,
+    Icon,
+    Input,
+    Label,
+    Row,
+    Spinner,
+    TabContent,
+    TabPane,
+    Toast,
+    ToastBody,
+    ToastHeader } from 'sveltestrap';
+  import FetchUrl from './FetchUrl.svelte';
+  import FetchFile from './FetchFile.svelte';
+  import FetchSoF from './FetchSoF.svelte';
+  import ResourceSelector from './ResourceSelector.svelte';
+  import { verify } from './shc-decoder.js';
 
-    import issuerKeys from './issuer.private.jwks.json';
-    import type { SHCFile,
-      Bundle,
-      SHCRetrieveEvent,
-      ResourceRetrieveEvent,
-      IPSRetrieveEvent,
-      SHLSubmitEvent, 
-      SOFAuthEvent} from './types';
-    import { page } from '$app/stores';
-    import { getResourcesFromIPS } from './resourceUploader.js';
-    import { goto } from '$app/navigation';
-    
-    let shlIdParam = $page.url.searchParams.get('shlid');
-    let tabParam = $page.url.searchParams.get('tab');
+  import issuerKeys from './issuer.private.jwks.json';
+  import type { SHCFile,
+    Bundle,
+    SHCRetrieveEvent,
+    ResourceRetrieveEvent,
+    IPSRetrieveEvent,
+    SHLSubmitEvent, 
+    SOFAuthEvent} from './types';
+  import { page } from '$app/stores';
+  import { getResourcesFromIPS } from './resourceUploader.js';
+  import { goto } from '$app/navigation';
+  
+  let shlIdParam = $page.url.searchParams.get('shlid');
 
-    const shlDispatch = createEventDispatcher<{ 'shl-submitted': SHLSubmitEvent }>();
-    let submitting = false;
-    let fetchError = "";
-    let currentTab: string | number;
-    currentTab = tabParam ?? 'url';
+  const shlDispatch = createEventDispatcher<{ 'shl-submitted': SHLSubmitEvent }>();
+  let submitting = false;
+  let fetchError = "";
+  let currentTab: string | number;
+  currentTab = 'url';
+  let addDataHeader = "Create IPS";
+  let addDataOpen = true;
+  let successMessage = false;
 
+  let resourceResult: ResourceRetrieveEvent = {
+    resources: undefined
+  }
+  let shcResult: SHCRetrieveEvent = {
+    shc: undefined
+  }
+  let ipsResult: IPSRetrieveEvent = {
+    ips: undefined
+  }
 
-    let resourceResult: ResourceRetrieveEvent = {
-      resources: undefined
+  let resourcesToReview: any[] = [];
+  let shcsToAdd: SHCFile[] = [];
+  let singleIPS = true;
+
+  let label = 'SHL from ' + new Date().toISOString().slice(0, 10);
+  let expiration: number | null = -1;
+  let type = 'password';
+  let showPassword = false;
+  let passcode = "";
+  $: type = showPassword ? 'text' : 'password';
+  $: icon = showPassword ? 'eye-slash-fill' : 'eye-fill';
+
+  onMount(() => {
+    if (sessionStorage.getItem('AUTH_REDIRECT')) {
+      sessionStorage.removeItem('AUTH_REDIRECT');
+      addDataHeader = "Add More Data";
     }
-    let shcResult: SHCRetrieveEvent = {
-      shc: undefined
-    }
-    let ipsResult: IPSRetrieveEvent = {
-      ips: undefined
-    }
-
-    let resourcesToReview: any[] = [];
-    let shcsToAdd: SHCFile[] = [];
-    let singleIPS = true;
-
-    let label = 'SHL from ' + new Date().toISOString().slice(0, 10);
-    let expiration: number | null = -1;
-    let type = 'password';
-    let showPassword = false;
-    let passcode = "";
-    $: type = showPassword ? 'text' : 'password';
-    $: icon = showPassword ? 'eye-slash-fill' : 'eye-fill';
-
-    onMount(() => {
-      if (sessionStorage.getItem('URL')) {
-        let url = sessionStorage.getItem('URL') ?? '/create';
-        sessionStorage.removeItem('URL');
+    if (sessionStorage.getItem('URL')) {
+      let url = sessionStorage.getItem('URL') ?? '/create';
+      let currentUrl = window.location.href.split('?')[0];
+      sessionStorage.removeItem('URL');
+      if (url !== currentUrl) {
         return goto(url);
       }
-      currentTab = sessionStorage.getItem('TAB') ?? currentTab;
-      label = sessionStorage.getItem('LABEL') ?? label;
-      passcode = sessionStorage.getItem('PASSCODE') ?? passcode;
-      if (sessionStorage.getItem('RESOURCES')) {
-        resourcesToReview = JSON.parse(sessionStorage.getItem('RESOURCES') ?? "") ?? resourcesToReview;
+    }
+    currentTab = sessionStorage.getItem('TAB') ?? currentTab;
+    label = sessionStorage.getItem('LABEL') ?? label;
+    passcode = sessionStorage.getItem('PASSCODE') ?? passcode;
+    if (sessionStorage.getItem('RESOURCES')) {
+      resourcesToReview = JSON.parse(sessionStorage.getItem('RESOURCES') ?? "") ?? resourcesToReview;
+    }
+    if (sessionStorage.getItem('EXPIRE')) {
+      expiration = JSON.parse(sessionStorage.getItem('EXPIRE') ?? "-1");
+    }
+    sessionStorage.removeItem('RESOURCES');
+    sessionStorage.removeItem('TAB');
+    sessionStorage.removeItem('LABEL');
+    sessionStorage.removeItem('PASSCODE');
+    sessionStorage.removeItem('EXPIRE');
+    const accordion = document.querySelector('div.add-data > div.accordion-collapse');
+    if (accordion) {
+      accordion.style.overflow = 'visible';
+    }
+    document.querySelector('a.nav-link.active')?.classList.remove('active');
+    document.querySelector('div.tab-pane.active')?.classList.remove('active')
+    document.querySelector('div.tab-pane.show')?.classList.remove('show')
+    document.querySelector(`span.${currentTab}-tab`)?.parentElement?.classList.add('active');
+    document.querySelector(`div.tab-pane.${currentTab}-tab`)?.classList.add('active');
+    document.querySelector(`div.tab-pane.${currentTab}-tab`)?.classList.add('show');
+  });
+
+  async function handleNewResources(details: ResourceRetrieveEvent) {
+      try {
+        resourceResult = details;
+        if (resourceResult.resources) {
+          // Trigger update in ResourceSelector
+          resourcesToReview = resourceResult.resources;
+          let resources = resourcesToReview.length > 0;
+          // dispatch('toggle', { resources });
+          const dataAccordion = document.querySelector('div.add-data > h2 > button');
+          if (dataAccordion) {
+            dataAccordion.click();
+          }
+          addDataHeader = resources ? "Add More Data" : "Create IPS";
+          showSuccessMessage();
+        }
+      } catch (e) {
+        console.log('Failed', e);
+        fetchError = "Error preparing IPS";
       }
-      if (sessionStorage.getItem('EXPIRE')) {
-        expiration = JSON.parse(sessionStorage.getItem('EXPIRE') ?? "-1");
+  }
+
+  async function handleSHCResultUpdate(details: SHCRetrieveEvent) {
+      try {
+        shcResult = details;
+        if (shcResult.shc && isSHCFile(shcResult.shc)) {
+          const decoded = await verify(shcResult.shc.verifiableCredential[0]);
+          const data = decoded.fhirBundle;
+          stageRetrievedIPS({ips: data, source: shcResult.source});
+        } else {
+          throw Error("Empty or invalid smart health card");
+        }
+
+      } catch (e) {
+        console.log('Failed', e);
+        fetchError = "Error processing health card";
       }
-      sessionStorage.removeItem('RESOURCES');
-      sessionStorage.removeItem('TAB');
-      sessionStorage.removeItem('LABEL');
-      sessionStorage.removeItem('PASSCODE');
-      sessionStorage.removeItem('EXPIRE');
+  }
+
+  async function uploadRetrievedIPS(details: IPSRetrieveEvent) {
+    try {
+      submitting = true;
+      ipsResult = details;
+      if (ipsResult.ips) {
+        shcsToAdd.unshift(await packageSHC(ipsResult.ips));
+        submitSHL();
+      }
+    } catch (e) {
+      submitting = false;
+      console.log('Failed', e);
+      fetchError = "Error submitting IPS";
+    }
+  }
+
+  async function stageRetrievedIPS(details: IPSRetrieveEvent) {
+    try {
+      if (singleIPS && ipsResult.ips) {
+        singleIPS = false;
+      }
+      ipsResult = details;
+      if (!ipsResult.ips) {
+        throw Error("Empty IPS content: " + JSON.stringify(details));
+      }
+
+      let ipsResources = getResourcesFromIPS(ipsResult.ips);
+      handleNewResources({ resources: ipsResources });
+    } catch (e) {
+      console.log('Failed', e);
+      fetchError = "Error parsing IPS";
+    }
+  }
+
+  async function preAuthRedirectHandler(details: SOFAuthEvent|undefined) {
+    sessionStorage.setItem('AUTH_REDIRECT', "1");
+    sessionStorage.setItem('URL', window.location.href);
+    sessionStorage.setItem('RESOURCES', JSON.stringify(resourcesToReview ?? ""));
+    sessionStorage.setItem('TAB', String(currentTab ?? ""));
+    sessionStorage.setItem('LABEL', label ?? "");
+    sessionStorage.setItem('PASSCODE', passcode ?? "");
+    sessionStorage.setItem('EXPIRE', JSON.stringify(expiration ?? -1));
+  }
+  
+  function isSHCFile(object: any): object is SHCFile {
+    return 'verifiableCredential' in object;
+  }
+
+  async function packageSHC(content:SHCFile | Bundle | undefined): Promise<SHCFile> {
+      if (content != undefined && isSHCFile(content) && content.verifiableCredential) {
+        return content;
+      }
+
+      const shc = await signJws(content);
+
+      return { verifiableCredential: [shc] };
+  }
+
+  async function submitSHL() {
+    return shlDispatch('shl-submitted', {
+      shcs: shcsToAdd,
+      label,
+      passcode: passcode ?? undefined,
+      exp: expiration && expiration > 0 ? new Date().getTime() / 1000 + expiration : undefined
     });
+  }
 
-    async function handleNewResources(details: ResourceRetrieveEvent) {
-        try {
-          resourceResult = details;
-          if (resourceResult.resources) {
-            // Trigger update in ResourceSelector
-            resourcesToReview = resourceResult.resources;
+  const exampleSigningKey = jose.importJWK(issuerKeys.keys[0]);
+  async function signJws(payload: unknown) {
+    const fields = { zip: 'DEF', alg: 'ES256', kid: issuerKeys.keys[0].kid };
+    const body = pako.deflateRaw(
+      JSON.stringify({
+        iss: 'https://spec.smarthealth.cards/examples/issuer',
+        nbf: new Date().getTime() / 1000,
+        vc: {
+          type: ['https://smarthealth.cards#health-card'],
+          credentialSubject: {
+            fhirVersion: '4.0.1',
+            fhirBundle: payload
           }
-        } catch (e) {
-          console.log('Failed', e);
-          fetchError = "Error preparing IPS";
         }
-    }
+      })
+    );
 
-    async function handleSHCResultUpdate(details: SHCRetrieveEvent) {
-        try {
-          shcResult = details;
-          if (shcResult.shc && isSHCFile(shcResult.shc)) {
-            const decoded = await verify(shcResult.shc.verifiableCredential[0]);
-            const data = decoded.fhirBundle;
-            stageRetrievedIPS({ips: data, source: shcResult.source});
-          } else {
-            throw Error("Empty or invalid smart health card");
-          }
+    const signed = new jose.CompactSign(body)
+    .setProtectedHeader(fields)
+    .sign(await exampleSigningKey);
+    return signed;
+  }
 
-        } catch (e) {
-          console.log('Failed', e);
-          fetchError = "Error processing health card";
-        }
-    }
-
-    async function uploadRetrievedIPS(details: IPSRetrieveEvent) {
-      try {
-        submitting = true;
-        ipsResult = details;
-        if (ipsResult.ips) {
-          shcsToAdd.unshift(await packageSHC(ipsResult.ips));
-          submitSHL();
-        }
-      } catch (e) {
-        submitting = false;
-        console.log('Failed', e);
-        fetchError = "Error submitting IPS";
-      }
-    }
-
-    async function stageRetrievedIPS(details: IPSRetrieveEvent) {
-      try {
-        if (singleIPS && ipsResult.ips) {
-          singleIPS = false;
-        }
-        ipsResult = details;
-        if (!ipsResult.ips) {
-          throw Error("Empty IPS content: " + JSON.stringify(details));
-        }
-
-        let ipsResources = getResourcesFromIPS(ipsResult.ips);
-        handleNewResources({ resources: ipsResources });
-      } catch (e) {
-        console.log('Failed', e);
-        fetchError = "Error parsing IPS";
-      }
-    }
-
-    async function preAuthRedirectHandler(details: SOFAuthEvent|undefined) {
-      sessionStorage.setItem('URL', window.location.href);
-      sessionStorage.setItem('RESOURCES', JSON.stringify(resourcesToReview ?? ""));
-      sessionStorage.setItem('TAB', String(currentTab ?? ""));
-      sessionStorage.setItem('LABEL', label ?? "");
-      sessionStorage.setItem('PASSCODE', passcode ?? "");
-      sessionStorage.setItem('EXPIRE', JSON.stringify(expiration ?? -1));
-    }
-    
-    function isSHCFile(object: any): object is SHCFile {
-      return 'verifiableCredential' in object;
-    }
-
-    async function packageSHC(content:SHCFile | Bundle | undefined): Promise<SHCFile> {
-        if (content != undefined && isSHCFile(content) && content.verifiableCredential) {
-          return content;
-        }
-
-        const shc = await signJws(content);
-
-        return { verifiableCredential: [shc] };
-    }
-
-    async function submitSHL() {
-      return shlDispatch('shl-submitted', {
-        shcs: shcsToAdd,
-        label,
-        passcode: passcode ?? undefined,
-        exp: expiration && expiration > 0 ? new Date().getTime() / 1000 + expiration : undefined
-      });
-    }
-
-    const exampleSigningKey = jose.importJWK(issuerKeys.keys[0]);
-    async function signJws(payload: unknown) {
-        const fields = { zip: 'DEF', alg: 'ES256', kid: issuerKeys.keys[0].kid };
-        const body = pako.deflateRaw(
-            JSON.stringify({
-                iss: 'https://spec.smarthealth.cards/examples/issuer',
-                nbf: new Date().getTime() / 1000,
-                vc: {
-                    type: ['https://smarthealth.cards#health-card'],
-                    credentialSubject: {
-                        fhirVersion: '4.0.1',
-                        fhirBundle: payload
-                    }
-                }
-            })
-        );
-
-        const signed = new jose.CompactSign(body)
-        .setProtectedHeader(fields)
-        .sign(await exampleSigningKey);
-        return signed;
-    }
+  async function showSuccessMessage() {
+    successMessage = true;
+    setTimeout(() => {
+      successMessage = false;
+    }, 1000);
+  }
 </script>
 
-<TabContent on:tab={(e) => {
-  currentTab = e.detail;
-}}>
-  <TabPane tabId="url" style="padding-top:10px" active={currentTab == "url"}>
-    <span slot="tab">FHIR URL</span>
-    <FetchUrl
-      on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }
-      on:ips-retrieved={ async ({ detail }) => { stageRetrievedIPS(detail) } }>
-    </FetchUrl>
-  </TabPane>
-  <TabPane tabId="file" style="padding-top:10px" active={currentTab == "file"}>
-    <span slot="tab">File Upload</span>
-    <FetchFile
-      on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }
-      on:ips-retrieved={ async ({ detail }) => { stageRetrievedIPS(detail) } }>
-    </FetchFile>
-  </TabPane>
-  <TabPane tabId="smart" style="padding-top:10px" active={currentTab == "smart"}>
-    <span slot="tab">SMART Patient Access</span>
-    <FetchSoF
-      on:sofAuthEvent={ async ({ detail }) => { preAuthRedirectHandler(detail) } }
-      on:updateResources={ async ({ detail }) => { handleNewResources(detail) } }
-      on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }>
-    </FetchSoF>
-  </TabPane>
-</TabContent>
+<Accordion>
+  <AccordionItem
+    active={resourcesToReview.length == 0}
+    header={addDataHeader}
+    class="add-data"
+    on:toggle={({ detail }) => {
+      addDataOpen = detail;
+      const accordion = document.querySelector('div.add-data > div.accordion-collapse');
+      if (accordion) {
+        accordion.style.overflow = 'hidden';
+      } else {
+        setTimeout(function() {
+          const accordion = document.querySelector('div.add-data > div.accordion-collapse');
+          if (accordion) {
+            accordion.style.overflow = 'visible';
+          }
+        }, 300);
+      }
+      
+    }}
+  >
+    <TabContent on:tab={(e) => {
+      currentTab = e.detail;
+    }}>
+      <TabPane class="url-tab" tabId="url" style="padding-top:10px" active>
+        <span class="url-tab" slot="tab" >FHIR URL</span>
+        <FetchUrl
+          on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }
+          on:ips-retrieved={ async ({ detail }) => { stageRetrievedIPS(detail) } }>
+        </FetchUrl>
+      </TabPane>
+      <TabPane class="file-tab" tabId="file" style="padding-top:10px">
+        <span class="file-tab" slot="tab">File Upload</span>
+        <FetchFile
+          on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }
+          on:ips-retrieved={ async ({ detail }) => { stageRetrievedIPS(detail) } }>
+        </FetchFile>
+      </TabPane>
+      <TabPane class="smart-tab" tabId="smart" style="padding-top:10px">
+        <span class="smart-tab" slot="tab">SMART Patient Access</span>
+        <FetchSoF
+          on:sofAuthEvent={ async ({ detail }) => { preAuthRedirectHandler(detail) } }
+          on:updateResources={ async ({ detail }) => { handleNewResources(detail) } }
+          on:shc-retrieved={ async ({ detail }) => { handleSHCResultUpdate(detail) } }>
+        </FetchSoF>
+      </TabPane>
+    </TabContent>
+  </AccordionItem>
+</Accordion>
 
 {#if resourcesToReview.length > 0}
   {#if shlIdParam == null}
-  <br/>
+    <Row class="mt-4">
+      <Col class="mb-2">
+        <h2>Configure SHL</h2>
+      </Col>
+      <Col>
+        <Toast class="me-1" autohide isOpen={successMessage} color="success">
+          <ToastBody>Success</ToastBody>
+        </Toast>
+      </Col>
+    </Row>
     <FormGroup>
       <Label>New SHLink Label</Label>
       <Input type="text" bind:value={label} />
