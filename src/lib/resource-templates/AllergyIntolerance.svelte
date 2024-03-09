976684146116b5620ed710d830b271998addf625
Daniel Lorigan <daniellrgn@gmail.com>
2024-03-09 14:12:47 -0800

    Update allergy code display

--- src/lib/resource-templates/AllergyIntolerance.svelte
+++ src/lib/resource-templates/AllergyIntolerance.svelte
@@ -30,20 +30,16 @@
   criticality: {resource.criticality ?? 'unknown'}
 </Badge>
 {#if resource.code}
-  {#if resource.code.coding}  
-    <Badge color="primary">{resource.code.coding[0].system} : {resource.code?.coding[0].code}</Badge>
-    <br>
-    <strong>
-      {#if resource.code.coding[0].display}
-        {resource.code.coding[0].display}
-      {:else}
-        {resource.code.text ?? ""}
-      {/if}
-    </strong>
-    <br>
+  {#if resource.code.coding}
+    <Badge color="primary">{resource.code.coding[0].system} : {resource.code.coding[0].code}</Badge>
+    <br />
+    {#if resource.code.coding[0].display}
+      <strong>{resource.code.coding[0].display}</strong><br>
+    {:else if resource.code.text}
+      <strong>{resource.code.text}</strong><br>
+    {/if}
   {:else if resource.code.text}
-    <strong>{resource.code.text}</strong>
-    <br>
+    <strong>{resource.code.text}</strong><br>
   {/if}
 {/if}
 {resource.onsetDateTime ? `Since ${resource.onsetDateTime.split("T")[0]}` : ''}
