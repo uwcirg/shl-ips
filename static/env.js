1c0aa16cbec381c183b44d3c7e2d65cfd5d80d57
Daniel Lorigan <daniellrgn@gmail.com>
2024-03-21 14:46:49 -0700

    Add template for env.js

---
+++ static/env.js
@@ -0,0 +1,11 @@
+window.globalConfig = {
+    // VITE_SHOW_DEMO: "",
+    // VITE_EPIC_CLIENT_ID: "",
+    // VITE_CERNER_CLIENT_ID: "",
+    // VITE_API_BASE: "",
+    // VITE_VIEWER_BASE: "",
+    // VITE_INTERMEDIATE_FHIR_SERVER_BASE: "",
+    // VITE_SOF_CLIENT_ID: "",
+    // VITE_SOF_ISS: "",
+    // DEV_SERVER_PORT: "",
+}
