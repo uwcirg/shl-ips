84ad0a5faa6ceb045dca169813dfe47d18b93097
Daniel Lorigan <daniellrgn@gmail.com>
2024-03-29 16:30:48 -0700

    Check that session is valid on loading content

--- src/lib/SessionStatus.svelte
+++ src/lib/SessionStatus.svelte
@@ -8,42 +8,35 @@
   // Poll the OP iframe periodically
   let checkSession = setInterval(checkSessionStatus, 5000); // Adjust interval as needed
   
-  onMount(() => {    
+  onMount(() => {
     // Listen for messages from OP iframe
-    window.addEventListener('message', function(event) {
-      const cookies = new Map();
-      if (!("hasStorageAccess" in document)) {
-          return true;
-        }
-
-      let access = document.hasStorageAccess();
-      for (const cookie of document.cookie.split(";")) {
-        const [key, value] = cookie.split("=").map((value) => value.trim());
-        cookies.set(key, value);
-      }
-      if (event.origin === OIDC_BASE) {
-        var data = event.data;
-        if (data === 'changed') {
-          console.log('Session state changed.');
-          try {
-            sofClient.getClient().refresh();
-          } catch (e) {
-            console.error(e);
-            goto('/logout');
-          }
-
-        } else if (data === 'error') {
-          goto('/logout');
-        }
-      }
-    });
+    window.addEventListener('message', processStatus);
+    // Check session on load
+    checkSessionStatus();
   });
 
   function checkSessionStatus() {
     var opIframe = document.getElementById('opIframe');
     var message = `${sofClient.getClient().getState('clientId')} ${sofClient.getClient().getState('tokenResponse.session_state')}`;
     // Send message to OP iframe
-    opIframe.contentWindow.postMessage(message, OIDC_BASE);
+    opIframe?.contentWindow.postMessage(message, OIDC_BASE);
+  }
+
+  function processStatus(event) {
+    if (event.origin === OIDC_BASE) {
+      var data = event.data;
+      if (data === 'changed') {
+        console.log('Session state changed.');
+        try {
+          sofClient.getClient().refresh();
+        } catch (e) {
+          console.error(e);
+          goto('/logout');
+        }
+      } else if (data === 'error') {
+        goto('/logout');
+      }
+    }
   }
 </script>
 
