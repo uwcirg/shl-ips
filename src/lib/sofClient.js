b4b326858174c37f7167db1a67b039bda01a7806
Daniel Lorigan <daniellrgn@gmail.com>
2024-04-03 13:43:14 -0700

    Add system to identifier request

--- src/lib/sofClient.js
+++ src/lib/sofClient.js
@@ -97,7 +97,11 @@ export class SOFClient {
 
     async requestResources(resourceType) {
         let self = this;
-        let endpoint = (resourceType == 'Patient' ? 'Patient?identifier=' : `${resourceType}?_count=1000&_sort=-date&subject.identifier=`) + this.getPatientID();
+        let endpoint = (
+            resourceType == 'Patient' 
+            ? 'Patient?identifier=' 
+            : `${resourceType}?_count=1000&_sort=-date&subject.identifier=`
+            ) + "https://keycloak.ltt.cirg.uw.edu|" + this.getPatientID();
         return this.client.request(endpoint, { flat: true }).then((result) => {
             let resourcesToPass = [];
             if (Array.isArray(result)) {
