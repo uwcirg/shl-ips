b57241598408ca474343d9155855b052a020befa
Justin McReynolds <mcjustin@uw.edu>
2024-05-19 09:04:15 -0700

    Bolds DR category labels.

--- src/lib/resource-templates/AdvanceDirective.svelte
+++ src/lib/resource-templates/AdvanceDirective.svelte
@@ -14,12 +14,12 @@ Text:
 <br />
 -->
 
-Category:
+<b>Category:</b>
 {#if resource.category && resource.category[0] && resource.category[0].coding && resource.category[0].coding[0]}
   {resource.category[0].coding[0].display} (LOINC {resource.category[0].coding[0].code})
 {/if}
 <br />
-Type:
+<b>Type:</b>
 <!-- 42348-3 is for "Advance Directive"; per cthon, it's presence here is redundant w/ category above. -->
 {#if resource.type && resource.type.coding && resource.type.coding[0] && resource.type.coding[0].code != '42348-3'}
   {resource.type.coding[0].display} (LOINC {resource.type.coding[0].code})
@@ -28,32 +28,32 @@ Type:
   {resource.type.coding[1].display} (LOINC {resource.type.coding[1].code}).
 {/if}
 <br />
-Description:
+<b>Description:</b>
 {#if resource.description}
   {resource.description}
 {/if}
 <br />
-setId:
+<b>setId:</b>
 {#if resource.identifier && resource.identifier[0] && resource.identifier[0].system && resource.identifier[0].system == 'https://mydirectives.com/standards/terminology/namingSystem/setId'}
   {resource.identifier[0].value}
 {/if}
 <br />
-Version number:
+<b>Version number:</b>
 {#if resource.extension && resource.extension[0] && resource.extension[0].url && resource.extension[0].url == 'http://hl7.org/fhir/us/ccda/StructureDefinition/VersionNumber'}
   {resource.extension[0].valueInteger}
 {/if}
 <br />
-Date:
+<b>Date:</b>
 {#if resource.date}
   {resource.date}
 {/if}
 <br />
-Status:
+<b>Status:</b>
 {#if resource.status}
   {resource.status}
 {/if}
 <br />
-docStatus:
+<b>docStatus:</b>
 {#if resource.docStatus}
   {resource.docStatus}
 {/if}
@@ -65,7 +65,7 @@ docStatus:
 {#if resource.content}
   {#each resource.content as content}
     {#if content.attachment && content.attachment.data}
-      PDF present: <a href={"data:application/pdf;base64," + content.attachment.data} target="_blank" rel="noopener noreferrer">View</a>
+      <b>PDF present:</b> <a href={"data:application/pdf;base64," + content.attachment.data} target="_blank" rel="noopener noreferrer">View</a>
     {/if}
   {/each}
 {/if}
