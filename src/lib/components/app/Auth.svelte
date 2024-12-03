e10058f92a90d85ffdab856c571be62380cc7124
Daniel Lorigan <daniellrgn@gmail.com>
2024-12-02 22:21:13 -0800

    Initial stab at auth flow with custom redirect url

--- src/lib/components/app/Auth.svelte
+++ src/lib/components/app/Auth.svelte
@@ -1,19 +1,48 @@
 <script lang="ts">
-  import { getContext, onMount, setContext } from 'svelte';
+  import { onMount, setContext } from 'svelte';
   import { AuthService } from '$lib/utils/AuthService';
   import type { User } from 'oidc-client-ts';
+  import { goto } from '$app/navigation';
+  import { Button } from 'sveltestrap';
 
-  let authService = getContext('authService') as AuthService;
+  export let authService: AuthService | undefined = undefined;
 
-  let currentUser: User | null = null;
+  let currentUser: User | undefined;
   
   onMount(async () => {
     if (!authService) {
       authService = new AuthService();
-      setContext('authService', authService);
     }
-
-    currentUser = await authService.getUser();
+    let newUser: User | undefined;
+    try {
+      newUser = await authService.signinCallback();
+    } catch (error) {
+      console.warn("No authentication parameters found, checking for current user");
+    } finally {
+      currentUser = await authService.getUser() ?? undefined;
+      if (currentUser) {
+        let now = Date.now() / 1000;
+        if ((currentUser.expires_at ?? 0) < now) {
+          currentUser = await authService.renewToken() ?? undefined;
+        }
+      }
+      if (!currentUser) {
+        await authService.login();
+      } else {
+        if (newUser) {
+          let redirectUrl = authService.getRedirectUrl();
+          if (redirectUrl) {
+            goto(redirectUrl);
+          } else {
+            goto('/home');
+          }
+        }
+      }
+    }
   });
 
 </script>
+{#if currentUser}
+  <Button color="primary" on:click={async () => await authService.logout()}>Sign out</Button>
+  <slot />
+{/if}
