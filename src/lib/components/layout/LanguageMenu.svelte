15aff2f60ff27e934b281671b1c379c419e20ee3
Daniel Lorigan <daniellrgn@gmail.com>
2025-01-06 12:00:39 -0800

    Put language menu on separate line above others to declutter navbar

--- src/lib/components/layout/LanguageMenu.svelte
+++ src/lib/components/layout/LanguageMenu.svelte
@@ -10,9 +10,10 @@
 </script>
 
 <Dropdown nav inNavbar class="navbar-dropdown" size="sm" direction="down">
-  <DropdownToggle color="primary" nav caret>
-    <Icon name="globe2" />
-    {$locales[$locale].lang}
+  <DropdownToggle color="primary" nav>
+    <span style="font-size:small">
+      {$locales[$locale].lang} <Icon name="globe2" />
+    </span>
   </DropdownToggle>
   <DropdownMenu end style="height: 500px; overflow:auto">
     {#if $locales}
