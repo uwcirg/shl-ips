91faeb033ca5c6cdc16f7102b78f0c6a7354fad2
Justin McReynolds <mcjustin@uw.edu>
2025-01-14 11:39:22 -0800

    QuestionnaireResponse now displays the URI for the questionnaire, the source if it exists, and the authored datetime.

--- src/lib/components/resource-templates/QuestionnaireResponse.svelte
+++ src/lib/components/resource-templates/QuestionnaireResponse.svelte
@@ -18,31 +18,15 @@
   }
 </script>
 
-{#if resource.clinicalStatus || resource.verificationStatus}
-  <Badge color="primary">
-    {resource.clinicalStatus?.coding?.[0].code ?? ''}
-    {resource.clinicalStatus &&
-      resource.verificationStatus
-        ? '/'
-        : ''}
-    {resource.verificationStatus?.coding?.[0].code ?? ''}
-  </Badge>
-{/if}
-<Badge color={badgeColor(resource.criticality ?? '')}>
-  {resource.type ? `${resource.type} - ` : ''}
-  criticality: {resource.criticality ?? 'unknown'}
-</Badge>
-{#if resource.code}
-  {#if resource.code.coding}
-    <Badge color="primary">{resource.code.coding[0].system} : {resource.code.coding[0].code}</Badge>
+<strong>{resource.questionnaire}</strong>
+<br />
+Authored: {resource.authored}
+{#if resource.source}
+  {#if resource.source.display}
     <br />
-    {#if resource.code.coding[0].display}
-      <strong>{resource.code.coding[0].display}</strong><br>
-    {:else if resource.code.text}
-      <strong>{resource.code.text}</strong><br>
-    {/if}
-  {:else if resource.code.text}
-    <strong>{resource.code.text}</strong><br>
+    Source: {resource.source.display}
+  {:else}
+    <br />
+    Source: (unknown)
   {/if}
 {/if}
-{resource.onsetDateTime ? `Since ${resource.onsetDateTime.split("T")[0]}` : ''}
