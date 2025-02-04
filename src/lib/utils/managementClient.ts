97578315f68b0eec71a3a6fc939df870557ce1bc
Daniel Lorigan <daniellrgn@gmail.com>
2025-02-04 15:12:19 -0500

    Add fields for create config w/vs

--- src/lib/utils/managementClient.ts
+++ src/lib/utils/managementClient.ts
@@ -5,6 +5,9 @@ import type { AuthService } from './AuthService';
 
 interface ConfigForServer extends Pick<SHLAdminParams, 'passcode' | 'exp' | 'label'> {
   userId?: string;
+  patientId?: string;
+  pin?: string;
+  patientIdentifierSystem?: string;
 }
 
 export interface SHLAdminParams {
@@ -61,14 +64,20 @@ export class SHLClient {
 
   async createShl(config: ConfigForServer = {}): Promise<SHLAdminParams> {
     config.userId = (await this.auth.getProfile())?.sub;
-    const res = await fetch(`${API_BASE}/shl`, {
+    config.patientId = config.userId;
+    config.pin = config.passcode;
+    config.patientIdentifierSystem = "http://keycloak.ips-shl.ubu.dlorigan.dev.cirg.uw.edu";
+    // const res = await fetch(`${API_BASE}/shl`, {
+    const res = await fetch(`https://pancanadianio.ca::10245/myhealth-gateway/v1/patient-summary`, {
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
+        "Authorization": `Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJfTENVTExfanhlb2JQYmtBM3lIaUV0OHlMdzVDVUF5ODVFYnVmTE9xeXNJIn0.eyJleHAiOjE3Mzg3MDA0NTUsImlhdCI6MTczODY5OTI1NSwianRpIjoiMDI4NjJhZTUtM2I0OC00MGVkLWI0NDAtYWZkMmVlZjA5NThkIiwiaXNzIjoiaHR0cHM6Ly9wYW5jYW5hZGlhbmlvLmNhOjEwMTAwL2F1dGgvcmVhbG1zL3BzLWNhIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImRkZWFmMjNmLThhZDktNDNlMy1hMzExLWYyM2FiY2RmNjgzZCIsInR5cCI6IkJlYXJlciIsImF6cCI6Imd1ZXN0LXJlc3QtY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6ImU4ZmMzOGM5LTNlZTMtNGM3Zi1iZjk0LTllNzMxYTFhYTdiOSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSBDQUZFWC0yIiwiY2xpZW50SG9zdCI6IjE1LjIyMi4xMDguNDk6NTY3OCIsImNsaWVudElkIjoiZ3Vlc3QtcmVzdC1jbGllbnQiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1ndWVzdC1yZXN0LWNsaWVudCIsImNsaWVudEFkZHJlc3MiOiIxNS4yMjIuMTA4LjQ5OjU2NzgifQ.R2rwb_ToM7cYXrezvDB1vAwTdDNLqwZXTzFDEGX1MN8UWys_ln0QX2D3-AXFdP4RsiRtDRkMwboyTLTTb9_Kb5EaosCoN3T_U5qFefusWSs25j4LAsYXB3X-uKt_MHxjAfGzXoA8z5RyHcX4NpP2ruG3oKJptULT1ozptHgrxGEpwRCBrTGPHDmwIiPe2w6QMsw0Oo9gEf5iPQvbjIASIXMsVpJCFhUC2CTibpL7pCbernGAI-GTcyhN7lFmLvkZ-ItwTk7IhxeZFldu7mmi8ry8iwv1c7WvynoQ6qKgLyZTtMaNnAQfom45q-gVhHQFiYrQdcbFR2fWTNPnDISPXg`
       },
       body: JSON.stringify(config)
     });
     console.log(`Request: POST ${API_BASE}/shl`);
+    console.log(`Request: POST https://pancanadianio.ca::10245/myhealth-gateway/v1/patient-summary`);
     console.log("Request body: ", JSON.stringify(config));
     const shlink = await res.text();
     console.log("Response body: ", shlink);
