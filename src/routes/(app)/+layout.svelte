4477486835897795bf4039d4e12cdbb2c34a57db
daniellrgn <daniellrgn@gmail.com>
2025-02-18 22:22:16 -0800

    Fix unauthenticated page flash after logging in

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -121,118 +121,112 @@
 {#await currentUser}
   Authorizing...
 {:then}
-<Navbar class="sticky-top" color="light" light expand="md" style="border-bottom: 1px solid rgb(204, 204, 204);">
-  <NavbarBrand href="https://doh.wa.gov/" target="_blank">
-    <Row>
-      <Col>
-        <Image id="nav-image" alt="Washington State Department of Health Logo" src="/img/doh_logo_doh-black.png"/>
-      </Col>
-    </Row>
-  </NavbarBrand>
-  <NavbarToggler on:click={() => ($isOpen = !$isOpen)} />
-  <Collapse class="flex-column" isOpen={$isOpen} navbar expand="md" on:update={handleUpdate}>
-      <Nav class="ms-auto" navbar>
-        <LanguageMenu />
-      </Nav>
-      <Nav class="ms-auto" navbar>
-        <NavItem>
-          <NavLink href="/home">Home</NavLink>
-        </NavItem>
-        <Dropdown nav inNavbar class="navbar-dropdown" size="sm" direction="down">
-          <DropdownToggle color="primary" nav caret>My Links</DropdownToggle>
-          <DropdownMenu end style="max-height: 500px; overflow:auto">
-            <DropdownItem
-              on:click={() => {
-                goto("/create");
-              }}>
-              <Icon name="plus-lg" /> Add New SHLink
-            </DropdownItem>
-            {#if $shlStore.length > 0}
-              <DropdownItem divider />
-              <DropdownItem header>Your Saved SHLinks</DropdownItem>
-              {#each $shlStore as shl, i}
-                <DropdownItem
-                  on:click={() => {
-                  goto('/view/' + shl.id);
-                }}>{shl.label || `SHLink ${i + 1}`}</DropdownItem>
-              {/each}
-            {/if}
-          </DropdownMenu>
-        </Dropdown>
-        {#await authService.getProfile()}
-          <NavItem>
-            <NavLink on:click={() => authService.login()}><Icon name="person-circle"/> Sign In</NavLink>
-          </NavItem>
-        {:then profile}
-          {#if profile}
-            <Dropdown nav inNavbar class="navbar-dropdown" size="sm" direction="down">
-              <DropdownToggle color="primary" nav caret><Icon name="person-circle"/> Account</DropdownToggle>
-              <DropdownMenu end style="max-height: 500px; overflow:auto">
-                <DropdownItem header>Welcome, {profile.given_name ?? profile.preferred_username}</DropdownItem>
-                <DropdownItem
-                  on:click={() => {
-                    authService.logout();
-                  }}><Icon name="box-arrow-right"/> Sign Out</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem header>Demo Options</DropdownItem>
+  {#await authService.getProfile() then profile}
+    {#if profile}
+      <Navbar class="sticky-top" color="light" light expand="md" style="border-bottom: 1px solid rgb(204, 204, 204);">
+        <NavbarBrand href="https://doh.wa.gov/" target="_blank">
+          <Row>
+            <Col>
+              <Image id="nav-image" alt="Washington State Department of Health Logo" src="/img/doh_logo_doh-black.png"/>
+            </Col>
+          </Row>
+        </NavbarBrand>
+        <NavbarToggler on:click={() => ($isOpen = !$isOpen)} />
+        <Collapse class="flex-column" isOpen={$isOpen} navbar expand="md" on:update={handleUpdate}>
+            <Nav class="ms-auto" navbar>
+              <LanguageMenu />
+            </Nav>
+            <Nav class="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="/home">Home</NavLink>
+              </NavItem>
+              <Dropdown nav inNavbar class="navbar-dropdown" size="sm" direction="down">
+                <DropdownToggle color="primary" nav caret>My Links</DropdownToggle>
+                <DropdownMenu end style="max-height: 500px; overflow:auto">
                   <DropdownItem
                     on:click={() => {
-                      $mode = ($mode === 'advanced' ? 'normal' : 'advanced');
-                  }}>
-                    <Row class="mr-0" style="min-width:240px">
-                      <Col class="d-flex justify-content-start align-items-center pe-0">
-                        {$mode === "advanced" ? "Hide" : "Show"} Advanced Features
-                      </Col>
-                      <Col class="d-flex justify-content-end ps-0">
-                        {#if $mode == 'advanced'}
-                        <Icon class="text-primary" name="toggle-on"></Icon>
-                        {:else}
-                        <Icon class="text-secondary" name="toggle-off"></Icon>
-                        {/if}
-                      </Col>
-                    </Row>
+                      goto("/create");
+                    }}>
+                    <Icon name="plus-lg" /> Add New SHLink
                   </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          {:else}
-            <NavItem>
-              <NavLink on:click={() => authService.login()}><Icon name="person-circle"/> Sign In</NavLink>
-            </NavItem>
-          {/if}
-        {/await}
-      </Nav>
-  </Collapse>
-</Navbar>
-<Banner title="International Patient Summary Prototype"/>
+                  {#if $shlStore.length > 0}
+                    <DropdownItem divider />
+                    <DropdownItem header>Your Saved SHLinks</DropdownItem>
+                    {#each $shlStore as shl, i}
+                      <DropdownItem
+                        on:click={() => {
+                        goto('/view/' + shl.id);
+                      }}>{shl.label || `SHLink ${i + 1}`}</DropdownItem>
+                    {/each}
+                  {/if}
+                </DropdownMenu>
+              </Dropdown>
+              <Dropdown nav inNavbar class="navbar-dropdown" size="sm" direction="down">
+                <DropdownToggle color="primary" nav caret><Icon name="person-circle"/> Account</DropdownToggle>
+                <DropdownMenu end style="max-height: 500px; overflow:auto">
+                  <DropdownItem header>Welcome, {profile.given_name ?? profile.preferred_username}</DropdownItem>
+                  <DropdownItem
+                    on:click={() => {
+                      authService.logout();
+                    }}><Icon name="box-arrow-right"/> Sign Out</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem header>Demo Options</DropdownItem>
+                    <DropdownItem
+                      on:click={() => {
+                        $mode = ($mode === 'advanced' ? 'normal' : 'advanced');
+                    }}>
+                      <Row class="mr-0" style="min-width:240px">
+                        <Col class="d-flex justify-content-start align-items-center pe-0">
+                          {$mode === "advanced" ? "Hide" : "Show"} Advanced Features
+                        </Col>
+                        <Col class="d-flex justify-content-end ps-0">
+                          {#if $mode == 'advanced'}
+                          <Icon class="text-primary" name="toggle-on"></Icon>
+                          {:else}
+                          <Icon class="text-secondary" name="toggle-off"></Icon>
+                          {/if}
+                        </Col>
+                      </Row>
+                    </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </Nav>
+        </Collapse>
+      </Navbar>
+      <Banner title="International Patient Summary Prototype"/>
 
-<Row class="main-row">
-  <Col>
-    <slot />
-  </Col>
-</Row>
-<Row>
-  <Col style="margin-top: 20px; padding: 20px; border-top: 1px solid rgb(204, 204, 204);" >
-    <footer>
-      This demonstration shows how to create a 
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://docs.smarthealthit.org/smart-health-links/user-stories"
-      >
-        SMART Health Link
-      </a>
-      for any FHIR
-      <a href="https://build.fhir.org/ig/HL7/fhir-ips/" target="_blank" rel="noreferrer">
-        International Patient Summary
-      </a>
-      document. SHLinks can be shared by copy/paste, or by presenting a QR code.
-      {#if $mode === "advanced"}
-        For more information, view the source code and license at
-        <a href="https://github.com/uwcirg/shl-ips" target="_blank" rel="noreferrer">
-          https://github.com/uwcirg/shl-ips
-        </a>. {VERSION_STRING ? "Site version: " + VERSION_STRING : ""}
-      {/if}
-    </footer>
-  </Col>
-</Row>
+      <Row class="main-row">
+        <Col>
+          <slot />
+        </Col>
+      </Row>
+      <Row>
+        <Col style="margin-top: 20px; padding: 20px; border-top: 1px solid rgb(204, 204, 204);" >
+          <footer>
+            This demonstration shows how to create a 
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://docs.smarthealthit.org/smart-health-links/user-stories"
+            >
+              SMART Health Link
+            </a>
+            for any FHIR
+            <a href="https://build.fhir.org/ig/HL7/fhir-ips/" target="_blank" rel="noreferrer">
+              International Patient Summary
+            </a>
+            document. SHLinks can be shared by copy/paste, or by presenting a QR code.
+            {#if $mode === "advanced"}
+              For more information, view the source code and license at
+              <a href="https://github.com/uwcirg/shl-ips" target="_blank" rel="noreferrer">
+                https://github.com/uwcirg/shl-ips
+              </a>. {VERSION_STRING ? "Site version: " + VERSION_STRING : ""}
+            {/if}
+          </footer>
+        </Col>
+      </Row>
+    {:else}
+      Authorizing...
+    {/if}
+  {/await}
 {/await}
