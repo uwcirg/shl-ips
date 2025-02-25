68ccbd3b102d20d45a2d79e1655943e4711a7be1
daniellrgn <daniellrgn@gmail.com>
2025-02-24 17:22:45 -0800

    Fix extra /create accordion opening animation on load

--- src/lib/components/app/AddFile.svelte
+++ src/lib/components/app/AddFile.svelte
@@ -153,6 +153,10 @@
     const accordion = document.querySelector('div.add-data > div.accordion-collapse');
     if (accordion) {
       accordion.style.overflow = 'visible';
+      accordion.classList.add('at-load');
+      setTimeout(() => {
+        accordion.classList.remove('at-load');
+      }, 250);
     }
 
     document.querySelector(`span.${currentTab}-tab`)?.parentElement?.click();
@@ -462,3 +466,9 @@
   <em class="text-secondary">* Advanced features for demo purposes only</em>
   <br>
 {/if}
+
+<style>
+  :global(.at-load) {
+    transition: all 0s !important;
+  }
+</style>
