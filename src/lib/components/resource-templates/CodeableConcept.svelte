0848f97e2359377ea3956982078a874bb77636d0
daniellrgn <daniellrgn@gmail.com>
2025-04-17 16:47:18 -0700

    Fix check in codeable concept template

--- src/lib/components/resource-templates/CodeableConcept.svelte
+++ src/lib/components/resource-templates/CodeableConcept.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Badge} from 'sveltestrap';
   import type { CodeableConcept } from "fhir/r4";
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
   export let codeableConcept: CodeableConcept; // Define a prop to pass the data to the component
   export let badge = true;
@@ -23,20 +23,18 @@
   })
 </script>
 
-{#if codeableConcept}
-  {#if codeableConcept.coding.length > 0}
-    {#if badge}
-      <Badge color="primary">{codeableConcept.coding[0].system} : {codeableConcept.coding[0].code}</Badge>
-      <br>
-    {/if}
-    {#if codeSet.size > 0}
-      {#each [...codeSet] as code, index}
-        {#if index === 0 && bold}
-          <strong>{code}</strong><br>
-        {:else}
-          {code}<br>
-        {/if}
-      {/each}
-    {/if}
+{#if codeableConcept?.coding?.length > 0}
+  {#if badge}
+    <Badge color="primary">{codeableConcept.coding[0].system} : {codeableConcept.coding[0].code}</Badge>
+    <br>
+  {/if}
+  {#if codeSet.size > 0}
+    {#each [...codeSet] as code, index}
+      {#if index === 0 && bold}
+        <strong>{code}</strong><br>
+      {:else}
+        {code}<br>
+      {/if}
+    {/each}
   {/if}
 {/if}
