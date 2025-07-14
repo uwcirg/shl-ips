68f8fec21608316122dd22e09aed903711eb9ad1
daniellrgn <daniellrgn@gmail.com>
2025-07-14 13:25:33 -0700

    Update POLST info page paragraph

--- src/lib/components/app/IntroductionPOLST.svelte
+++ src/lib/components/app/IntroductionPOLST.svelte
@@ -2,5 +2,5 @@
   "WA Health Summary | POLST" lets people obtain and personally control the sharing of their POLST advanced care planning documents and other health information, with any health care provider, family member or other individuals of their choosing using a simple web link or QR code.
 </p>
 <p class="info-paragraph">
-  POLST forms let people with serious illnesses describe the treatments that match their values and preferences, to ensure they do not receive treatments they don’t want, and that they do receive treatments they value.
+  This version of WA Health Summary is limited to the use of Portable Orders for Life Sustaining Treatment (POLST).  These forms let people with serious illnesses describe the treatments that match their values and preferences, to ensure they do not receive treatments they don’t want, and that they do receive treatments they value.
 </p>
