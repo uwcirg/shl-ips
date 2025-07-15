53e9aa45bb814b15fea8e10be765c8330a8ca987
daniellrgn <daniellrgn@gmail.com>
2025-07-15 13:13:47 -0700

    Actually fix patient name in template

--- src/lib/components/viewer/IPSContent.svelte
+++ src/lib/components/viewer/IPSContent.svelte
@@ -92,7 +92,8 @@
     }
     let patient = ips.entry?.filter((entry) => entry.resource?.resourceType === 'Patient').map((entry) => entry.resource);
     if (patient?.[0]) {
-      let patientName = patient[0].name?.[0]?.text ?? `${(patient[0].name?.[0]?.prefix ?? "") (patient[0].name?.[0]?.given?.join(' ') ?? "") (patient[0].name?.[0]?.family ?? "")}`;
+      let patientName = patient[0].name?.[0]?.text ??
+        `${patient[0].name?.[0]?.prefix ?? ""} ${patient[0].name?.[0]?.given?.join(' ') ?? ""} ${patient[0].name?.[0]?.family ?? ""}`;
       content["Patient"] = {
         section: {
           text: {
