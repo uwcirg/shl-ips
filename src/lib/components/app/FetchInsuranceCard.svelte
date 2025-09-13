e4ce7788f0329d3a238028d651f2b28aa67aadff
Justin McReynolds <mcjustin@uw.edu>
2025-09-12 23:53:53 -0400

    Fix sveltestrap syntax

--- src/lib/components/app/FetchInsuranceCard.svelte
+++ src/lib/components/app/FetchInsuranceCard.svelte
@@ -6,7 +6,7 @@
     Input,
     Label,
     Row,
-    Spinner } from 'sveltestrap';
+    Spinner } from '@sveltestrap/sveltestrap';
 
   import type { InsurcardRetrieveEvent } from '$lib/utils/types';
   import { createEventDispatcher } from 'svelte';
