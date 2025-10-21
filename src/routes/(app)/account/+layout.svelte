69e597975d65258cbd54cc69269f0a4cfd9a79a5
daniellrgn <daniellrgn@gmail.com>
2025-10-21 15:29:58 -0700

    Add responsive account menu matching data page

--- src/routes/(app)/account/+layout.svelte
+++ src/routes/(app)/account/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-  import { Nav, NavLink, NavItem, Collapse, Row, Col } from 'sveltestrap';
+  import {
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Icon,
+    Nav,
+    NavLink,
+    NavItem,
+  } from 'sveltestrap';
   import { goto } from '$app/navigation';
   
   let tabs: Record<string, string> = {
@@ -16,25 +25,42 @@
     }
   }
 
+  let tabNavOpen: boolean = false;
   function toggleTab(tabId: string) {
     activeTab = tabId;
   }
 
 </script>
 
-<h4>Account Settings</h4>
 <div class="d-flex flex-column flex-lg-row tab-content data-tabs">
-  <!-- Sidebar for medium and up -->
+  <!-- Sidebar for large and up -->
   <Nav pills vertical style="width:15em" class="flex-column d-none d-lg-flex me-3 p-2 bg-light rounded">
     <h6 class="py-2">Account Settings</h6>
     {#each Object.entries(tabs) as [tabId, tabName]}
       <NavItem>
-        <NavLink active={activeTab === tabId} on:click={() => { toggleTab(tabId); goto(`/account/${tabId}`); }}>
+        <NavLink active={activeTab === tabId} on:click={() => { toggleTab(tabId) }}>
           {tabName}
         </NavLink>
       </NavItem>
     {/each}
   </Nav>
+  <!-- Mobile navigation toggle -->
+  <div class="d-lg-none mb-3">
+    <Nav tabs>
+      <NavItem>
+        <Dropdown nav isOpen={tabNavOpen}>
+          <DropdownToggle nav caret class="active">Account <Icon name="chevron-right" style="font-size: .7em"/> {tabs[activeTab]}</DropdownToggle>
+          <DropdownMenu>
+            {#each Object.entries(tabs) as [tabId, tabName]}
+              <DropdownItem active={activeTab === tabId} on:click={() => toggleTab(tabId)}>
+                  <span>{tabName}</span>
+              </DropdownItem>
+            {/each}
+          </DropdownMenu>
+        </Dropdown>
+      </NavItem>
+    </Nav>
+  </div>
   <div class="tab-pane active show w-100">
     <slot />
   </div>
