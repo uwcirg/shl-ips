9adf6ff75b8af78bf13edf4e36427f50216ea12c
daniellrgn <daniellrgn@gmail.com>
2025-10-29 15:01:40 -0700

    Don't prerender routes that have been excluded by the instance

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-node';
+import { INSTANCE_CONFIG } from './src/lib/config/instance_config';
+
+const disallowedRoutes = INSTANCE_CONFIG.disallowedRoutes.map((r) => `/${r}`);
 
 const dev = process.argv.includes('dev');
 
@@ -21,6 +24,20 @@ export default {
         'script-src': ['self'],
         'report-uri': ['/']
       }
+    },
+    prerender: {
+      handleHttpError: ({ path, status, message }) => {
+        // ignore 404s on disallowed routes
+        if (disallowedRoutes.find((r) => path.startsWith(r)) && status === 404) {
+          console.log(`Skipping prerender of disallowed path: ${path}`);
+          return; // don't fail the build
+        }
+        throw new Error(`${status} on ${path}: ${message}`);
+      },
+      entries: [
+        '/', // homepage
+        ...allowedRoutes, // only prerender allowed routes
+      ]
     }
   }
 };
