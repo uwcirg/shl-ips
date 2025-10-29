4d51a1a67a1330df897f629c547549e06e65a6b7
daniellrgn <daniellrgn@gmail.com>
2025-10-29 15:04:21 -0700

    Add MyHealthSummary instance config for prod

--- src/lib/config/instance_config.ts
+++ src/lib/config/instance_config.ts
@@ -1,3 +1,4 @@
+import MyHealthSummary from '$lib/config/config_MyHealthSummary';
 import WAHealthSummary from '$lib/config/config_WAHealthSummary';
 import WAHealthSummaryPOLST from '$lib/config/config_WAHealthSummaryPOLST';
 import WAHealthSummaryPOLSTProvider from '$lib/config/config_WAHealthSummaryPOLSTProvider';
@@ -5,9 +6,10 @@ import WAHealthSummaryPOLSTProvider from '$lib/config/config_WAHealthSummaryPOLS
 const INSTANCE_ID = import.meta.env.VITE_INSTANCE_ID;
 
 const configs: Record<string, any> = {
-  WAHealthSummary: WAHealthSummary,
-  WAHealthSummaryPOLST: WAHealthSummaryPOLST,
-  WAHealthSummaryPOLSTProvider: WAHealthSummaryPOLSTProvider
+  "MyHealthSummary": MyHealthSummary,
+  "WAHealthSummary": WAHealthSummary,
+  "WAHealthSummaryPOLST": WAHealthSummaryPOLST,
+  "WAHealthSummaryPOLSTProvider": WAHealthSummaryPOLSTProvider
 };
 
 export const INSTANCE_CONFIG = configs[INSTANCE_ID] || WAHealthSummary;
