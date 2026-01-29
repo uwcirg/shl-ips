bb8b34a2c9d0ad0255c8f934ea727c87f7bc1f70
Daniel Lorigan <dlorigan@uw.edu>
2026-01-29 13:35:21 -0800

    Introduction component fixes

--- src/lib/components/app/Introduction.svelte
+++ src/lib/components/app/Introduction.svelte
@@ -1,7 +1,10 @@
+<script lang='ts'>
+  import { INSTANCE_CONFIG } from '$lib/config/instance_config';
+</script>
 <h2 class="page-title">Your health information, in your hands</h2>
 <p class="info-paragraph">
-  Welcome to WA Health Summary, the Washington State Department of Health's platform for creating and sharing your personal health summaries.
+  Welcome to {INSTANCE_CONFIG.title}, the Washington State Department of Health's platform for creating and sharing your personal health summaries.
 </p>
 <p class="info-paragraph">
-  WA Health Summary lets people gather their health information and personally control sharing of their health information with health care providers, family members or others, using a simple, secure web link or QR code.
+  {INSTANCE_CONFIG.title} lets people gather their health information and personally control sharing of their health information with health care providers, family members or others, using a simple, secure web link or QR code.
 </p>
--- src/lib/components/app/IntroductionMyHealthSummary.svelte
+++ src/lib/components/app/IntroductionMyHealthSummary.svelte
@@ -1,6 +1,5 @@
-<script>
-    import { INSTANCE_CONFIG } from "$lib/config/instance_config";
-
+<script lang='ts'>
+  import { INSTANCE_CONFIG } from "$lib/config/instance_config";
 </script>
 <h2 class="page-title">Your health information, in your hands.</h2>
 <p class="info-paragraph">
@@ -8,3 +7,4 @@
 </p>
 <p class="info-paragraph">
   {INSTANCE_CONFIG.title} lets people gather their health information and personally control sharing of their health information with health care providers, family members or others, using a simple, secure web link or QR code.
+</p>
