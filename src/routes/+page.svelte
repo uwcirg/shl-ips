96c768fcf8422f185abf1914ba5d701aa69b3b74
Daniel Lorigan <daniellrgn@gmail.com>
2026-02-06 14:23:41 -0800

    Update text content 260126 (#115)
    * Add configurable privacy FAQ
    
    * Remove mispaced $
    
    * Additional updates to faq text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,7 +28,7 @@
 
 <svelte:component this={INSTANCE_CONFIG.pages?.home?.introComponent} />
 
-<Row class="my-4 d-flex justify-content-center">
+<Row class="mt-4 mb-5 d-flex justify-content-center">
   <Row class="d-flex justify-content-center">
     {#if $authenticated}
       <Col class="d-flex align-items-center px-2">
@@ -79,36 +79,21 @@
     <AccordionItem>
       <h5 slot="header">How did {INSTANCE_CONFIG.title} begin?</h5>
       <p class="info-paragraph">
-        Begining in 2021, WA State Deparment of Health's WA Verify program made it easy for people receiving COVID-19 vaccinations in
-        Washington State to access and share that information using SMART Health Card QR codes.
-        {INSTANCE_CONFIG.title} builds on the ideas and technologies of WA Verify, which was used by almost
-        two million people from 2021-2023.
+        {INSTANCE_CONFIG.title} builds on the ideas and technologies of WA State Deparment of Health's WA Verify program that began in 2021, which made it easy for people receiving COVID-19 vaccinations in Washington State to access and share that information digitally, using SMART Health Card (SHC) QR codes. WA Verify was used by nearly two million people from 2021-2023.
       </p>
 
       <p class="info-paragraph">
-        Now, using SMART Health Links, {INSTANCE_CONFIG.title} enables personally-controlled access to,
-        and sharing of, health data by WA State residents to meet needs such as travel, emergency
-        records access, school enrollment, etc.
+        Now, using SMART Health Links, {INSTANCE_CONFIG.title} enables personally-controlled access to, and sharing of, health data by WA State residents to meet needs such as travel, emergency records access, school enrollment, etc.
       </p>
 
       <p class="info-paragraph">
-        {INSTANCE_CONFIG.title} lets people access, augment, and electively share their own health data
-        using the International Patient Summary (IPS) standard. They might choose to do so to
-        verify immunizations, to summarize traveler health information, to have a medical summary
-        in the event they require urgent or emergency care, or for routine sharing of their full
-        history with others.
+        Now, using SMART Health Links (SHLs), {INSTANCE_CONFIG.title} lets WA State residents access, augment, and electively share their own health data using the International Patient Summary (IPS) standard. They might choose to do so to verify immunizations, to summarize traveler health information, to have a medical summary in the event they require urgent or emergency care, or for routine sharing of their full history with others.
       </p>
     </AccordionItem>
     <AccordionItem>
       <h5 slot="header">What is the International Patient Summary?</h5>
       <p class="info-paragraph">
-        IPS information includes important clinical history, medications, immunizations, advance
-        directives, and other data vital for health care. This data may be helpful to those
-        traveling, to parents or caregivers, and to anyone who wants to be able to see their own
-        records, or securely share their data with healthcare providers or others of their
-        choosing. {INSTANCE_CONFIG.title} uses the secure, patient-controlled SMART Health Link (SHL)
-        standard to allow people to share information collected and stored on their behalf by the
-        Washington State Department of Health.
+        The International Patient Summary (IPS) is a standardized set of basic clinical data that includes important clinical history, medications, immunizations, advance directives, and other data vital for health care. This data may be helpful to those traveling, to parents or caregivers, and to anyone who wants to be able to see their own records, or to securely share their data with healthcare providers or others of their choosing. {INSTANCE_CONFIG.title} uses the secure, patient-controlled SMART Health Link (SHL) standard to allow people to share information collected and stored on their behalf by the Washington State Department of Health.
       </p>
     </AccordionItem>
   </Accordion>
