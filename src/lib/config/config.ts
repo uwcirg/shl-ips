73bdb65ee2b68112257ed4f83aa5e7dd53aaa62d
Daniel Lorigan <dlorigan@uw.edu>
2026-03-02 16:20:13 -0800

    Remove Epic IHE endpoint that was removed after projectathon

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -365,6 +365,5 @@ export const EXAMPLE_IPS = {
   'Desiree': 'https://fhir.ips-demo.dev.cirg.uw.edu/fhir/Patient/Pat1-System2/$summary',
   // 2025-01 Connectathon PACIO track:
   'Jenny Mosley': 'https://hapi.fhir.org/baseR4/Patient/patientJM1/$summary',
-  'Epic IHE 2026': 'https://pancanadianio.ca:10101/Interconnect-connectathon-ca/api/FHIR/R4/Patient/e6EjlmN1AGE9X.2NGyMV-GQ3/$summary?_format=json',
 };
 export const IPS_DEFAULT = 'Maria SEATTLE Gravitate';
