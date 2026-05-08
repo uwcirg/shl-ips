08e387c718e66961b9c8d6c7a4152c4eb172923d
Daniel Lorigan <dlorigan@uw.edu>
2026-05-07 23:56:49 -0700

    Home button to tabs, icons on shrink, remove home link

--- src/lib/components/layout/TabNav.svelte
+++ src/lib/components/layout/TabNav.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Nav, NavItem, NavLink } from '@sveltestrap/sveltestrap';
+  import { Icon, Nav, NavItem, NavLink } from '@sveltestrap/sveltestrap';
   export let activeItem: string;
   let className = '';
   export { className as class };
@@ -9,14 +9,31 @@
 
 <div class="border container-fluid d-flex align-items-center justify-content-center p-2 rounded-4 {classes}" style="background-color: white;">
   <Nav pills class="nav-justified w-100">
-    <NavItem class="mx-1">
-      <NavLink href="/data" active={ activeItem === "data" } class="d-flex justify-content-center rounded-5 text-nowrap">Add/Import</NavLink>
+    <NavItem class="mx-1 flex-grow-0" style="width: min-content !important">
+      <NavLink href="/home" active={activeItem === "home"} class="d-flex align-items-center justify-content-center rounded-5 px-3 py-0" style="line-height: 1; height: 100%">
+        <Icon class="fs-4" name="house"/>
+      </NavLink>
     </NavItem>
     <NavItem class="mx-1">
-      <NavLink href="/data/manage" active={ activeItem === "manage" } class="d-flex justify-content-center rounded-5">Review</NavLink>
+      <NavLink href="/data" active={activeItem === "data"} class="d-flex flex-column flex-sm-row justify-content-center align-items-center rounded-5 text-nowrap gap-1">
+        <Icon class="d-sm-none fs-6" name="building-add"/>
+        <span class="d-none d-sm-inline">Add/Import</span>
+        <small class="d-sm-none" style="font-size: 0.6rem; line-height: 1.2">Add Data</small>
+      </NavLink>
     </NavItem>
     <NavItem class="mx-1">
-      <NavLink href="/summaries" active={ activeItem === "summaries" || activeItem === "share" } class="d-flex justify-content-center rounded-5">Share</NavLink>
+      <NavLink href="/data/manage" active={activeItem === "manage"} class="d-flex flex-column flex-sm-row justify-content-center align-items-center rounded-5 gap-1">
+        <Icon class="d-sm-none fs-6" name="clipboard-check"/>
+        <span class="d-none d-sm-inline">Review</span>
+        <small class="d-sm-none" style="font-size: 0.6rem; line-height: 1.2">Review</small>
+      </NavLink>
+    </NavItem>
+    <NavItem class="mx-1">
+      <NavLink href="/summaries" active={activeItem === "summaries" || activeItem === "share"} class="d-flex flex-column flex-sm-row justify-content-center align-items-center rounded-5 gap-1">
+        <Icon class="d-sm-none fs-6" name="share"/>
+        <span class="d-none d-sm-inline">Share</span>
+        <small class="d-sm-none" style="font-size: 0.6rem; line-height: 1.2">Share</small>
+      </NavLink>
     </NavItem>
   </Nav>
 </div>
