89ea6199082cd3144a0f3fd36946f1ef465e61af
Daniel Lorigan <dlorigan@uw.edu>
2026-05-07 17:37:14 -0700

    Clean up shlStore updating in a couple of places

--- src/lib/components/app/HealthLink.svelte
+++ src/lib/components/app/HealthLink.svelte
@@ -219,11 +219,7 @@
   }
 
   async function deleteFile(fileContent:string) {
-    shl = await shlClient.deleteFile(shl, fileContent).then((shl) => {
-      let updatedFiles = shl.files.filter((f) => f.contentHash !== fileContent);
-      shl.files = updatedFiles;
-      return shl;
-    });
+    shl = await shlClient.deleteFile(shl, fileContent);
     $shlStore = await shlClient.getUserShls();
   }
 </script>
