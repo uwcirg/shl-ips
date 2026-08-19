4e947f1506dcff9a18401bc2a7a2c425f0fef2d6
Daniel Lorigan <daniellrgn@gmail.com>
2026-08-18 18:27:24 -0700

    Make changes to intro paragraphs outlined in this document: https://docs.google.com/document/d/1qdUi6bAcXI-aDIFGjIq0UITWhnOB90P-/edit#heading=h.k7wkf7e5fpq9 (#175)

--- src/lib/components/app/Introduction.svelte
+++ src/lib/components/app/Introduction.svelte
@@ -3,8 +3,8 @@
 </script>
 <h2 class="page-title">Your health information, in your hands</h2>
 <p class="info-paragraph">
-  Welcome to {INSTANCE_CONFIG.title}, the Washington State Department of Health's platform for creating and sharing your personal health summaries.
+  Welcome to {INSTANCE_CONFIG.title}, a patient-centered platform for securely creating and sharing your personalized health summaries. This demonstration system requires creating an account to see the experience for a unique individual, but should not be used for your actual health data.
 </p>
 <p class="info-paragraph">
-  {INSTANCE_CONFIG.title} lets people gather their health information and personally control sharing of their health information with health care providers, family members or others, using a simple, secure web link or QR code.
+  {INSTANCE_CONFIG.title} lets you gather and organize your health information, and share any or all of that information with health care providers, family members, or others, using a simple, secure web link or QR code. You remain in full control of anything you choose to share.
 </p>
